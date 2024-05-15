9f722cb0ebea770299a37ed87750a556f8153d48
Robin Collins <Robin.F.Collins@outlook.com>
2024-05-15 14:32:24 +0930

    chore: Update API URL in App component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <API apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml" />
+      <API apiDescriptionUrl="https://raw.githubusercontent.com/robin-collins/ppdb/main/src/openapi.yaml" />
     </div>
   );
 }
